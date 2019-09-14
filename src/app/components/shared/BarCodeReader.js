bcc35fce7ea58dfc9b9de3e14a9eac566e13d5ca
Shantanu Kamdi <kamdishantanu@gmail.com>
2019-09-14 20:12:06 +0530

    Add Abhyasi  and barcode code

---
+++ src/app/components/shared/BarCodeReader.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import Quagga from 'quagga';
+
+export default class BarCodeReader extends Component {
+  constructor(props) {
+    super(props);
+    this._onSuccess = this._onSuccess.bind(this);
+    this._onFailure = this._onFailure.bind(this);
+  }
+
+  componentDidMount() {
+    Quagga.init(
+      {
+        inputStream: {
+          name: 'Live',
+          type: 'LiveStream',
+          target: document.querySelector('#interactive') // Or '#yourElement' (optional)
+        },
+        decoder: {
+          readers: ['code_128_reader']
+        },
+        numOfWorkers: 0,
+        multiple: false
+      },
+      function(err) {
+        if (err) {
+          console.log(err);
+
+          return;
+        }
+        console.log('Initialization finished. Ready to start');
+        Quagga.start();
+      }
+    );
+
+    Quagga.onDetected(this._onSuccess);
+  }
+
+  componentWillUnmount() {
+    Quagga.offDetected(this._onSuccess);
+    Quagga.stop();
+  }
+
+  _onSuccess(result) {
+    this.props.onSuccess(result);
+  }
+
+  _onFailure(error) {
+    this.props.onFailure(error);
+  }
+
+  render() {
+    return <div id="interactive" className="viewport" />;
+  }
+}
--- src/app/components/visitor/abhyasi/Abhyasi.js
+++ src/app/components/visitor/abhyasi/Abhyasi.js
@@ -1,11 +1,102 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Button, Form, Select } from 'semantic-ui-react';
+
+import BarCodeReader from '../../shared/BarCodeReader';
+import u from '../../../libs/utils';
 
 export default class Abhyasi extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scanning: false,
+      abhyasiBarCode: '',
+      inTime: null
+    };
+    this._scan = this._scan.bind(this);
+    this._onDetected = this._onDetected.bind(this);
+    this._handleScanBarcode = this._handleScanBarcode.bind(this);
+    this._onFailure = this._onFailure.bind(this);
+  }
+
+  _scan() {
+    this.setState({ scanning: !this.state.scanning });
+  }
+
+  _onDetected(result) {
+    this.setState({
+      abhyasiBarCode: result.codeResult.code,
+      inDate: new Date()
+    });
+    this._scan();
+    u.log('Scanned result', this.state.abhyasiBarCode);
+  }
+
+  _onFailure(err) {
+    u.error('Error in scanning', err);
+  }
+
+  _handleScanBarcode(event) {
+    this._onDetected(event.target.value);
+  }
+
+  handleChange = event => {
+    this.setState({ abhyasiBarCode: event.target.value });
+  };
+
+  handleFormSubmit = () => {
+    this.setState({ inDate: new Date() });
+    firebaseAppDflt
+      .firestore()
+      .collection('visitors')
+      .doc('Abhyasi')
+      .collection('data')
+      .add({
+        abhyasiBarCode: this.state.abhyasiBarCode,
+        inTime: Date.now()
+      })
+      .then(value => {
+        console.log('Document added successfully');
+        this.setState({ abhyasiBarCode: '', inDate: null });
+      });
+  };
+
   render() {
+    const options = [
+      { key: 'af', value: 'af', text: 'Afghanistan' },
+      { key: 'ax', value: 'ax', text: 'Aland Islands' }
+    ];
     return (
       <div>
-        <Header as="h4">Abhyasi</Header>
+        <Button onClick={this._scan}>
+          {this.state.scanning ? 'Stop Scanning' : 'Start Scanning'}
+        </Button>
+
+        <br />
+
+        {this.state.scanning ? (
+          <BarCodeReader
+            onSuccess={this._onDetected}
+            onFailure={this._onFailure}
+          />
+        ) : null}
+        <br />
+        <Form onSubmit={this.handleFormSubmit}>
+          <Form.Field>
+            <label>Scanned Barcode</label>
+            <input
+              placeholder="Barcode Reader output will appear here"
+              value={this.state.abhyasiBarCode}
+              onChange={this.handleChange}
+              required
+            />
+            <br />
+            <br />
+            <Select placeholder="Select Operation" options={options} />
+            <br />
+            <br />
+            <Button primary>Submit</Button>
+          </Form.Field>
+        </Form>
       </div>
     );
   }
