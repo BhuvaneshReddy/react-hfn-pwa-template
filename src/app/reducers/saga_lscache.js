552f6c65da2111296943abf6132b0feb19e53b2b
Sree Hari <nsreehari@gmail.com>
2019-10-30 07:49:31 +0530

    updated boilerplate

--- src/app/reducers/saga_lscache.js
+++
@@ -1,9 +0,0 @@
-import { put } from 'redux-saga/effects';
-import { LSC_FLUSH_CACHE } from './constants';
-
-
-export function* flushCache({ db }) {
-  yield put({ type: LSC_FLUSH_CACHE, db });
-}
-
-
